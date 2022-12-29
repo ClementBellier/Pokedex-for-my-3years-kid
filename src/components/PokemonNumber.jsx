cbfa3cdb54913959e3f63c0129afe0ffe96a58ec
Clement BELLIER <clement.bellier@gmail.com>
2022-12-29 16:02:09 +0100

    Build after react refacto

--- src/components/PokemonNumber.jsx
+++
@@ -1,8 +0,0 @@
-const DisplayPokemonNumber = ({pokemonNumber}) =>{
-    const numberToString = pokemonNumber.toString()
-    const pad = "000"
-    const pokemonNumberDisplay = pad.substring(0, pad.length - numberToString.length) + pokemonNumber
-    return (<div id="pokemonNumber">{pokemonNumberDisplay}</div>)
-}
-
-export default DisplayPokemonNumber
--- src/components/SpeakIcon.jsx
+++
@@ -1,9 +0,0 @@
-const SpeakIcon = () => {
-  return (
-    <div className="icone speak-icone">
-      <i className="fa-solid fa-volume-high"></i>
-    </div>
-  );
-};
-
-export default SpeakIcon;
